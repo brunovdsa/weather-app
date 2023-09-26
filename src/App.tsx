271d87e5eb1cf07262b86f8a28ffd43205232339
Bruno de Sá <brunoviniciussouza.sa@gmail.com>
2023-09-26 17:07:51 -0300

    FEAT: CSS updated.

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,40 @@ import { LocationIcon } from './components/Icons/icons';
 function App() {
   return (
     <div className='container'>
-      <main className='box'>
-        <section>
-          <div className='header'>
-            <h1 className='location'>
-              <span>
-                <LocationIcon className='location-icon' />
-              </span>
-              Joinville, SC
-            </h1>
-            <p>Segunda, 25 de setembro 16:56 </p>
-          </div>
-          <div className='content'>
-            <p className='temperature'>20ºC</p>
-            <img
-              src='https://openweathermap.org/img/wn/10d@2x.png'
-              alt='Weather icon.'
-              className='weather-icon'
-            />
-          </div>
-        </section>
-        <p>Drizzle</p>
-        <p>Feels like 21</p>
-        <p>temperatura minima</p>
-        <p>temperatura max</p>
-        <p>humidade</p>
-        <p className='weather-temp'></p>
-      </main>
+      <div className='box'>
+        <main>
+          <article>
+            <div className='header'>
+              <h1 className='location'>
+                <span>
+                  <LocationIcon className='location-icon' />
+                </span>
+                Joinville, SC
+              </h1>
+              <p>Segunda, 25 de setembro 16:56 </p>
+            </div>
+            <div className='content'>
+              <p className='temperature'>20º</p>
+              <div className='subheading'>
+                <div className='icon-container'>
+                  <p className='weather-condition'>Drizzle</p>
+                  <img
+                    src='https://openweathermap.org/img/wn/10d@2x.png'
+                    alt='Weather icon.'
+                    className='weather-icon'
+                  />
+                </div>
+              </div>
+              <div className='weather-info'>
+                div.
+                <p>temperatura minima</p>
+                <p>temperatura max</p>
+                <p>humidade</p>
+              </div>
+            </div>
+          </article>
+        </main>
+      </div>
     </div>
   );
 }
